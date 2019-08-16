e88a599cb5a34ff2017e80ae881ae2a6b45f50ed
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-08-16 17:27:52 +0530

    Modify test to work cross platform

--- tests/test.js
+++ tests/test.js
@@ -3,10 +3,32 @@ const { expect } = require('chai');
 const { ProcessName, ProcessNameConstants } = require('../index');
 
 
+const getChrome = () => {
+  if (process.platform === 'darwin') {
+    return 'Chrome';
+  }
+
+  if (process.platform === 'win32') {
+    return 'chrome.exe';
+  }
+
+  return 'chrome';
+};
+
+
+const getFirefox = () => {
+  if (process.platform === 'win32') {
+    return 'firefox.exe';
+  }
+
+  return 'firefox';
+};
+
+
 describe('tests process names', () => {
   it('should return correct process name', () => {
-    expect(ProcessName.BROWSERS[ProcessNameConstants.BROWSERS.CHROME][process.platform]).to.equal('Chrome');
-    expect(ProcessName.BROWSERS[ProcessNameConstants.BROWSERS.FIREFOX][process.platform]).to.equal('firefox');
+    expect(ProcessName.BROWSERS[ProcessNameConstants.BROWSERS.CHROME][process.platform]).to.equal(getChrome());
+    expect(ProcessName.BROWSERS[ProcessNameConstants.BROWSERS.FIREFOX][process.platform]).to.equal(getFirefox());
   });
   it('should return undefined for invalid application', () => {
     expect(ProcessName.BROWSERS[ProcessNameConstants.BROWSERS.ARSHAD]).to.equal(undefined);
